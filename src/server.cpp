f72c8ef301da933367f4a43dbe3f52309af34de1
Shashidhar Biradar <shashidharb459@gmail.com>
2025-06-15 17:34:06 +0530

    deployment commit

--- src/server.cpp
+++ src/server.cpp
@@ -8,7 +8,8 @@ Server::Server(int port, const std::string& config_path)
 }
 
 void Server::start() {
-    wsClient.connect("ws://localhost:5000");
+    // wsClient.connect("ws://localhost:5000");
+    wsClient.connect("wss://backend-deltax.onrender.com/ws"); 
     router.setupRouteHandler(svr);
     spdlog::info("API Gateway listening on port {}", port);
     svr.listen("0.0.0.0", port);
